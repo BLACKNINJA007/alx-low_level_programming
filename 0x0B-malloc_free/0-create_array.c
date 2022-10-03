ee2613516c5fe24ecedad6aa03848c75c0b10fc3
BLACKNINJA007 <torojonah05@gmail.com>
2022-10-03 15:35:55 -0700

    0-create_array.c correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
-
 	array = (char *)malloc(sizeof(char) * size);
+
 	if (array == NULL)
 		return (NULL);
 
