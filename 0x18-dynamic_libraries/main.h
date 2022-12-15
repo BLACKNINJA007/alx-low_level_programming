671dae0eb3f1e559cbbc136808822ab9dcb3a771
BLACKNINJA007 <torojonah05@gmail.com>
2022-12-15 03:21:02 -0800

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _ MAIN_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,4 +30,4 @@ int mul(int a, int b);
 int mod(int a, int b);
 extern div_t div(int __numer, int __denom);
 
-#endif
+#endif/*MAIN_H*/
